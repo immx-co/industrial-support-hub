eb051df592fd0676800a62e32fedaa60f52d3884
Vyacheslav <slava.orlov.2001@mail.ru>
2026-07-19 12:52:42 +0300

    Initialize industrial support hub monorepo

---
+++ support-service/src/main/java/com/immx/industrialsupport/supportservice/SupportServiceApplication.java
@@ -0,0 +1,15 @@
+package com.immx.industrialsupport.supportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SupportServiceApplication.class,
+                args);
+    }
+
+}
